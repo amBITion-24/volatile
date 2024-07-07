84eb2e4236a5e3401b747bf5a9c8b53c6690a40d
Ankit Sharma <152326291+Ankitsharma2023@users.noreply.github.com>
2024-07-07 11:28:19 +0530

    Update Admin.js
    frontend correction in admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,11 +2,11 @@ import React from "react";
 import ApprovePass from "./ApprovePass";
 import CheckAllPassStatus from "./CheckAllPassStatus";
 
-const Admin = () => {
+const Consumer = () => {
   return (
     <div className="bg-blue-400 my-10 rounded-md p-4 flex flex-col items-center lg:w-[90vw] m-auto">
       <div className="w-full text-4xl font-semibold text-center text-white py-4 mb-5 rounded-lg bg-gray-700">
-        Admin
+        Consumer
       </div>
 
       <div className="flex gap-10 max-xl:flex-col justify-between w-full items-center">
@@ -17,4 +17,4 @@ const Admin = () => {
   );
 };
 
-export default Admin;
+export default Consumer;
