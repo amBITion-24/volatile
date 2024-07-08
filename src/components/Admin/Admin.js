81a61d55c1f4245866a09238c145f48dde699ce1
Ankit Sharma <152326291+Ankitsharma2023@users.noreply.github.com>
2024-07-08 13:07:36 +0530

    changes some bugs

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ApprovePass from "./ApprovePass";
+//made some changes
 import CheckAllPassStatus from "./CheckAllPassStatus";
 
 const Consumer = () => {
   return (
     <div className="bg-white my-10 rounded-md p-4 flex flex-col items-center lg:w-[90vw] m-auto">
       <div className="w-full text-4xl font-semibold text-center text-black py-4 mb-5 rounded-lg bg-white border border-black">
-          Consumer
+        Consumer
       </div>
       <div className="flex gap-10 max-xl:flex-col justify-between w-full items-center">
         <CheckAllPassStatus />
         <ApprovePass />
       </div>
     </div>
-    
   );
 };
 
